c20198c148547fa60a0511505a8ee5fd741167e4
nnguyen <carrie.he1704@gmail.com>
2017-11-03 21:39:24 +0700

    adding Dtos for data communication.

---
+++ 2C2PAssignment/2C2PAssignment.Business/Dtos/ExpriedDateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2C2PAssignment.Business.Dtos
+{
+    public class ExpriedDateData
+    {
+        public int Month { get; set; }
+
+        public int Year { get; set; }
+    }
+}
