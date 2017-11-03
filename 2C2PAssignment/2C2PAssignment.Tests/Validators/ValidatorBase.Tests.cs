e95d23739cc1ff750e7c0726e1ca8c4549c70588
nnguyen <carrie.he1704@gmail.com>
2017-11-03 22:44:08 +0700

    add testcase check valid Expiry date and fix gammar naming problem

--- 2C2PAssignment/2C2PAssignment.Tests/Validators/ValidatorBase.Tests.cs
+++ 2C2PAssignment/2C2PAssignment.Tests/Validators/ValidatorBase.Tests.cs
@@ -17,7 +17,7 @@ namespace _2C2PAssignment.Tests.Validators
         [TestCase(null)]
         [TestCase("")]
         [TestCase("    ")]
-        public void Validate_GivenCardNumberNullOrEmptyWithValidExperiedDate_Should_ReturnInvalidAndUnknownCard(string cardNumber)
+        public void Validate_GivenCardNumberNullOrEmptyWithValidExpiriedDate_Should_ReturnInvalidAndUnknownCard(string cardNumber)
         {
             //Arr
             ValidatorBase val = new ValidatorBase();
@@ -36,7 +36,7 @@ namespace _2C2PAssignment.Tests.Validators
         [TestCase("123456789012345")]
         [TestCase("12345678901234567")]
         [TestCase("123456789012345123456789012345")]
-        public void Validate_GivenCardNumberMoreOrLessThan16WithValidExperiedDate_Should_ReturnInvalidAndUnknownCard(string cardNumber)
+        public void Validate_GivenCardNumberMoreOrLessThan16WithValidExpiriedDate_Should_ReturnInvalidAndUnknownCard(string cardNumber)
         {
             //Arr
             ValidatorBase val = new ValidatorBase();
@@ -57,7 +57,7 @@ namespace _2C2PAssignment.Tests.Validators
         [TestCase("123456b890123451")]
         [TestCase("c23456b890123451")]
         [TestCase("123456b89012345a")]
-        public void Validate_GivenCardNumberContainsNotDigitNumberWithValidExperiedDate_Should_ReturnInvalidAndUnknownCard(string cardNumber)
+        public void Validate_GivenCardNumberContainsNotDigitNumberWithValidExpiriedDate_Should_ReturnInvalidAndUnknownCard(string cardNumber)
         {
             //Arr
             ValidatorBase val = new ValidatorBase();
@@ -72,7 +72,7 @@ namespace _2C2PAssignment.Tests.Validators
         }
 
         [Test]
-        public void Validate_GivenValidCardNumberWithNullExperiedDate_Should_ReturnInvalidAndUnknownCard()
+        public void Validate_GivenValidCardNumberWithNullExpiriedDate_Should_ReturnInvalidAndUnknownCard()
         {
             //Arr
             ValidatorBase val = new ValidatorBase();
@@ -88,7 +88,11 @@ namespace _2C2PAssignment.Tests.Validators
 
         [Test]
         [TestCase(11, 2016)]
-        public void Validate_GivenValidCardNumberWithExperiedDate_Should_ReturnIsValidFalse(int month, int year)
+        [TestCase(10, 2017)]
+        [TestCase(9, 2017)]
+        [TestCase(8, 2017)]
+        [TestCase(7, 2017)]
+        public void Validate_GivenValidCardNumberWithExpiriedDate_Should_ReturnIsValidFalse(int month, int year)
         {
             //Arr
             ValidatorBase val = new ValidatorBase();
@@ -100,6 +104,27 @@ namespace _2C2PAssignment.Tests.Validators
             //Assert
             Assert.True(result != null);
             Assert.True((!result.IsValid));
+            Assert.True(result.Type == null);
+        }
+
+        [Test]
+        [TestCase(11, 2018)]
+        [TestCase(11, 2017)]
+        [TestCase(12, 2017)]
+        [TestCase(1, 2018)]
+        [TestCase(2, 2018)]
+        public void Validate_GivenValidCardNumberWithValidExpiryDate_Should_ReturnIsValidTrue(int month, int year)
+        {
+            //Arr
+            ValidatorBase val = new ValidatorBase();
+            SystemDatetime.Now = () => new DateTime(2017, 11, 3);
+
+            //Act
+            var result = val.Validate("1234567890123456", new ExpriedDateData() { Month = month, Year = year });
+
+            //Assert
+            Assert.True(result != null);
+            Assert.True((result.IsValid));
             Assert.True(result.Type == null);
         }
     }
