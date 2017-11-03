0f74f0631c9b68ba3e4c13342908b770f804c035
nnguyen <carrie.he1704@gmail.com>
2017-11-03 21:38:36 +0700

    adding business class and interface for card validation.

---
+++ 2C2PAssignment/2C2PAssignment.Business/Business/CardValidationBusiness.cs
@@ -0,0 +1,36 @@
+﻿using _2C2PAssignment.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2C2PAssignment.Business.Dtos;
+using _2C2PAssignment.Business.Validator;
+
+namespace _2C2PAssignment.Business.Business
+{
+    public class CardValidationBusiness : ICardValidationBusiness
+    {
+        public ValidateResultDto Validate(string cardNumber, ExpriedDateData date)
+        {
+            ValidateResultDto result;
+
+            if ((result = new JCBValidator().Validate(cardNumber, date)).Type != CardType.Unknown)
+            {
+                return result;
+            }
+            else if ((result = new VisaValidator().Validate(cardNumber, date)).Type != CardType.Unknown)
+            {
+                return result;
+            }
+            else if ((result = new VisaValidator().Validate(cardNumber, date)).Type != CardType.Unknown)
+            {
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+    }
+}
